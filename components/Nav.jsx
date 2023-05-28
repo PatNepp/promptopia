da91dab7603d5cef4fc75aeac231598a99b2ddc7
Patrick Nepp <ptnepp@gmail.com>
2023-05-27 19:37:12 -0500

    google auth, database, and user schemas created for sign in

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -6,18 +6,18 @@ import { useState, useEffect } from "react"
 import { signIn, signOut, useSession, getProviders } from 'next-auth/react'
 
 const Nav = () => {
-    const isUserLoggedIn = true
+    const { data: session } = useSession()
     const [providers, setProviders] = useState(null)
     const [toggleDropdown, setToggleDropdown] = useState(false)
 
     useEffect(() => {
-        const setProviders = async () => {
+        const setupProviders = async () => {
             const response = await getProviders()
 
             setProviders(response)
         }
 
-        setProviders()
+        setupProviders()
     }, [])
 
   return (
@@ -26,9 +26,10 @@ const Nav = () => {
             <Image src="/assets/images/logo.svg" alt='promptopia logo' height={30} width={30} className="object-contain"/>
             <p className="logo_text">Promptopia</p>
         </Link>
+
         {/* Desktop Navigation */}
         <div className="sm:flex hidden">
-            {isUserLoggedIn ? (
+            {session?.user ? (
                 <div className="flex gap-3 md:gap-5">
                     <Link href='/create-prompt' className="black_btn">
                         Create Post
@@ -37,7 +38,7 @@ const Nav = () => {
                         Sign Out
                     </button>
                     <Link href='/profile'>
-                        <Image src="/assets/images/logo.svg" width={37} height={37} className="rounded-full" alt='profile'/>
+                        <Image src={session?.user.image} width={37} height={37} className="rounded-full" alt='profile'/>
                     </Link>
                 </div>
             ) : (
@@ -53,9 +54,9 @@ const Nav = () => {
 
         {/* Mobile Navigation */}
         <div className="sm:hidden flex relative">
-            {isUserLoggedIn ? (
+            {session?.user ? (
                 <div className="flex">
-                    <Image src="/assets/images/logo.svg" width={37} height={37} className="rounded-full" alt='profile' onClick={() => setToggleDropdown((prev) => !prev)}/>
+                    <Image src={session?.user.image} width={37} height={37} className="rounded-full" alt='profile' onClick={() => setToggleDropdown((prev) => !prev)}/>
                     {toggleDropdown && (
                         <div className="dropdown">
                             <Link href='/profile' className="dropdown_link" onClick={() => setToggleDropdown(false)}>
